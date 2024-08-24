73baf8282ce550f47f53f4ae6785c12f8e3874f9
ErfinderR <60707176+ErfinderR@users.noreply.github.com>
2024-08-24 13:31:30 +0200

    Remove duplicate implementation and adjust license

--- test/source/bms.cpp
+++ test/source/bms.cpp
@@ -5,29 +5,7 @@
 #include <multipleint/mi.hpp>
 
 #include "../../benchmark/include/bench_targets.hpp"
-
-namespace detail
-{
-
-template<size_t, typename T>
-constexpr T&& identity(T&& x)
-{
-  return std::forward<T>(x);
-}
-
-template<typename T, size_t... Indices>
-constexpr auto array_repeat_impl(T&& x, std::index_sequence<Indices...>)
-{
-  return std::array {identity<Indices>(x)...};
-}
-
-}  // namespace detail
-
-template<size_t N, typename T>
-constexpr auto array_repeat(T&& x) -> std::array<T, N>
-{
-  return detail::array_repeat_impl(std::forward<T>(x), std::make_index_sequence<N>());
-}
+#include "../../benchmark/include/util.hpp"  // array_repeat
 
 TEST(Sum_red_alt, TwoIntEightFullPositiveValues)
 {
